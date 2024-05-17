747fd995de8987cf30240ff5f6fb0143b8850940
Badmus Qudus Ayomide <badmusqudusayomide@gmail.com>
2024-05-17 11:05:29 +0100

    changes

---
+++ Samueldare/script.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById("btn")
+let dark;
+let yellow;
+    btn.onclick = function () {
+        dark = document.getElementById("cover").style.color = "white";
+        yellow = document.getElementById("cover").style.backgroundColor =
+          "black";
+        no = document.getElementById("coverl").style.backgroundColor =
+          "black";
+        
+    }
+// let cover = document.getElementById("cover");
+
+// let dark;
+// btn.onclick = function () {
+//     cover.style.display="none"
+// }
